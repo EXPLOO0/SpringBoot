1df9644e4aebca583dd5ad9c0930924b69bdcaa7
EXPLOO0 <1534016557@QQ.COM>
2023-08-06 13:28:19 +0800

    2023/8/6-13.27---创建工程，导入mybatisplus依赖

---
+++ src/main/java/com/zytpro/naicha/NaichaApplication.java
@@ -0,0 +1,13 @@
+package com.zytpro.naicha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaichaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaichaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zytpro/naicha/NaichaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zytpro.naicha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaichaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
