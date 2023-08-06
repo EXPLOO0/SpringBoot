c2e94942a36c15ce917cfcdf4a01392c0051d8a3
EXPLOO0 <1534016557@QQ.COM>
2023-08-06 15:04:22 +0800

    2023/8/6-15：04---完成admin的Service层的登录功能

--- src/main/java/com/zytpro/naicha/service/AdminService.java
+++ src/main/java/com/zytpro/naicha/service/AdminService.java
@@ -1,9 +1,12 @@
 package com.zytpro.naicha.service;
 
+import com.zytpro.naicha.pojo.Admin;
+
 /**
  * @Description：admin数据表的Service层
  * @Author：ZYT
  * @Date：2023/8/6 13:40
  */
 public interface AdminService {
+    public Admin login(String account, String pwd);
 }
--- src/main/java/com/zytpro/naicha/service/impl/AdminServiceImpl.java
+++ src/main/java/com/zytpro/naicha/service/impl/AdminServiceImpl.java
@@ -1,11 +1,39 @@
 package com.zytpro.naicha.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.zytpro.naicha.mapper.AdminMapper;
+import com.zytpro.naicha.pojo.Admin;
 import com.zytpro.naicha.service.AdminService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * @Description：admin数据表的Service层的实现类
  * @Author：ZYT
  * @Date：2023/8/6 13:42
  */
+@Service
 public class AdminServiceImpl implements AdminService {
+
+    @Resource
+    AdminMapper adminMapper;
+
+    /**
+     * 接收用户名和密码，查找是否存在
+     * @param account：用户名
+     * @param pwd：密码
+     * @return：admin对象，找到了就返回admin对象，没找到就返回null
+     */
+    @Override
+    public Admin login(String account, String pwd) {
+        // 创建LambdaQueryWrapper条件构造器对象
+        LambdaQueryWrapper<Admin> lambdaQueryWrapper = new LambdaQueryWrapper<Admin>();
+        // 给条件构造器对象添加条件，判断用户名和密码
+        lambdaQueryWrapper.eq(Admin::getAAccount,account).eq(Admin::getAPwd,pwd);
+        // 使用MyBatisPlus的selectOne方法，将条件构造器对象作为参数查找一个对象
+        Admin admin = adminMapper.selectOne(lambdaQueryWrapper);
+
+        return admin;
+    }
 }
