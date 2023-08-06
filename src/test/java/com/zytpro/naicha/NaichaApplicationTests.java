2ac89ac4d77c1c0ed20d30f2cf20c9bc7284b6a9
EXPLOO0 <1534016557@QQ.COM>
2023-08-06 14:48:33 +0800

    2023/8/6-14.45---测试AdminMapper功能

--- src/test/java/com/zytpro/naicha/NaichaApplicationTests.java
+++ src/test/java/com/zytpro/naicha/NaichaApplicationTests.java
@@ -1,13 +1,28 @@
 package com.zytpro.naicha;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.zytpro.naicha.mapper.AdminMapper;
+import com.zytpro.naicha.pojo.Admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class NaichaApplicationTests {
 
+    @Resource
+    private AdminMapper adminMapper;
+
     @Test
     void contextLoads() {
+        LambdaQueryWrapper<Admin> lambdaQueryWrapper = new LambdaQueryWrapper<Admin>();
+        lambdaQueryWrapper.eq(Admin::getAAccount,"admin").eq(Admin::getAPwd,"admin123");
+
+        Admin admin = adminMapper.selectOne(lambdaQueryWrapper);
+//        List<Admin> admins = adminMapper.selectList(null);
+        System.out.println(admin);
     }
 
 }
