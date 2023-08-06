1df394ce76015e541cf7c1441c0df8c7d15d0370
EXPLOO0 <1534016557@QQ.COM>
2023-08-06 15:56:20 +0800

    2023/8/6-15：55---数据表type的数据层

---
+++ src/main/java/com/zytpro/naicha/mapper/TypeMapper.java
@@ -0,0 +1,14 @@
+package com.zytpro.naicha.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zytpro.naicha.pojo.Type;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Description：数据表type的数据层
+ * @Author：ZYT
+ * @Date：2023/8/6 15:45
+ */
+@Mapper
+public interface TypeMapper extends BaseMapper<Type> {
+}
